e2e03081ab781372bd82ae7b1b0ea359733310b9
poggersv2 <131923140+poggersv2@users.noreply.github.com>
2025-05-26 21:12:31 +1200

    Switch to tank drive control scheme
    default arcade drive removed. replaced with simple tank drive scheme

--- src/main.cpp
+++ src/main.cpp
@@ -75,20 +75,31 @@ void autonomous() {}
  */
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
+	// Change motor ports later
 	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
 	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
 
+	//remember to set the gearing of the motors here!!!!
 
 	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
+		// Tank Control Scheme
+		double left_speed = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+		double right_speed = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y)
 
-		// Arcade control scheme
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
-		left_mg.move(dir - turn);                      // Sets left motor voltage
-		right_mg.move(dir + turn);                     // Sets right motor voltage
-		pros::delay(20);                               // Run for 20 ms then update
+		left_mg.move(left_speed);
+		right_mg.move(right_speed);
+		pros::delay(20);
+
 	}
+
+
+
+
+	//while (true) {
+		//pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
+		               //  (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
+		                // (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
+
+		
+	//}
 }
